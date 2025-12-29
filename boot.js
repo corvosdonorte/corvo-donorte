bc0f5d5de2efabef54580ff95e7a2599c75a494d
corvo <corvosdonorte@gmail.com>
2025-12-29 01:37:47 -0300

    Delete boot.js

--- boot.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("btnLogin")?.addEventListener("click", loginGoogle);
-    document.getElementById("btnLogout")?.addEventListener("click", logoutGoogle);
-    document.getElementById("btnAdd")?.addEventListener("click", adicionarMembro);
-  });
-  
