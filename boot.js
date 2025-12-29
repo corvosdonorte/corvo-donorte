df34d09eba0e4b08d7ba2784313effa19f822021
corvo <corvosdonorte@gmail.com>
2025-12-29 01:34:43 -0300

    Add files via upload

---
+++ boot.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("btnLogin")?.addEventListener("click", loginGoogle);
+    document.getElementById("btnLogout")?.addEventListener("click", logoutGoogle);
+    document.getElementById("btnAdd")?.addEventListener("click", adicionarMembro);
+  });
+  
